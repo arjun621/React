f13cc784e1389f1a6ac9786e109b2d790ca7e25e
Arjun Parashar <arjunparashar488@gmail.com>
2026-01-16 11:10:11 +0530

    Updated

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+        <div className='card'>
+            <h1>Ajun Parashar</h1>
+            <p>Lorem, ipstionem ius molliqussumenda provident quos eveniet odit dolore sequi.</p>
+        </div>
+    </div>
+  )
+}
+
+export default card
---
+++ 03-props/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from './components/Card';
+
+const App = () => {
+  return (
+    <div className="parent">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default App;
+
+
+//1h 31min
